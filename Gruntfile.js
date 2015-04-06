96a83dc36aedf41512686bdef06db9f3ea5def97
hyneman <rico.beti@silentbyte.com>
2015-04-06 14:31:56 +0200

    Added font-awesome dependency.
    Also, font files that are installed via Bower (i.e. glyphicons and
    font-awesome) will now be ignored in Git since they can be created
    via package control.

--- Gruntfile.js
+++ Gruntfile.js
@@ -357,7 +357,8 @@ module.exports = function (grunt) {
 						cwd: '<%= yeoman.app %>/bower_components/',
 						dest: '<%= yeoman.app %>/styles/fonts/',
 						src: [
-							'bootstrap-sass-official/vendor/assets/fonts/bootstrap/**'
+							'bootstrap-sass-official/vendor/assets/fonts/bootstrap/**',
+							'components-font-awesome/fonts/**'
 						]
 					}
 				]
